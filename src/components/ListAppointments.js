e75c732cb12e3a21f08c90add25e530677e0730d
Tyler Reid <tylerreid1988@gmail.com>
2020-11-18 11:06:50 -0500

    add font-awesome

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { FaTimes } from "react-icons/fa";
 
 class ListAppointments extends Component {
     render() {
@@ -11,24 +12,29 @@ class ListAppointments extends Component {
       
         return (
             <div className="appointment-list item-list mb-3">
-            <div className="pet-item col media py-3">
-              <div className="mr-3">
-                <button className="pet-delete btn btn-sm btn-danger">X</button>
-              </div>
-  
-              <div className="pet-info media-body">
-                <div className="pet-head d-flex">
-                  <span className="pet-name">petName</span>
-                  <span className="apt-date ml-auto">aptDate</span>
-                </div>
-  
-                <div className="owner-name">
-                  <span className="label-item">Owner: </span>
-                  <span>ownerName</span>
-                </div>
-                <div className="apt-notes">aptNotes</div>
-              </div>
-            </div>
+                {this.props.appointments.map(item => (
+                    <div className="pet-item col media py-3" key={item.aptId}>
+                    <div className="mr-3">
+                      <button className="pet-delete btn btn-sm btn-danger">
+                          <FaTimes />
+                      </button>
+                    </div>
+        
+                    <div className="pet-info media-body">
+                      <div className="pet-head d-flex">
+                        <span className="pet-name">{item.petName}</span>
+                        <span className="apt-date ml-auto">{item.aptDate}</span>
+                      </div>
+        
+                      <div className="owner-name">
+                        <span className="label-item">Owner: </span>
+                        <span>{item.ownerName}</span>
+                      </div>
+                      <div className="apt-notes">aptNotes</div>
+                    </div>
+                  </div>
+                ))}
+            
         </div>
         );
     }
